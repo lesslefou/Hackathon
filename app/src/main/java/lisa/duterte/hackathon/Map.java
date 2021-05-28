c2c13a7061bf475e26fb305c65c237d39ea6e548
the-b <theo.soda@isen.yncrea.fr>
2021-05-28 13:37:22 +0200

    Ajout de l'activité MAP + Modif reset/change password

--- app/src/main/java/lisa/duterte/hackathon/Map.java
+++ app/src/main/java/lisa/duterte/hackathon/Map.java
@@ -1,14 +1,80 @@
 package lisa.duterte.hackathon;
 
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Map extends AppCompatActivity {
+public class Map extends AppCompatActivity implements OnMapReadyCallback {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        Button back = findViewById(R.id.backBtn);
+        back.setOnClickListener(v -> finish());
+
+        //Obtain the SupportMapFragment and get notified when the map is ready to be used
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //set the location of the drone
+        LatLng isen = new LatLng(43.12064347637604, 5.939666736157341);
+        googleMap.addMarker(new MarkerOptions()
+                .position(isen)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.drone_logo))
+                .title(String.valueOf(R.string.position_drone)));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(isen, 14));
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                logout();
+                return true;
+            case R.id.setting:
+                Intent i = new Intent(this, Setting.class);
+                startActivity(i);
+                return true;
+            case R.id.aboutUs:
+                Intent i1 = new Intent(this, AboutUs.class);
+                startActivity(i1);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void logout() {
+        //FirebaseAuth.getInstance().signOut();
+        Intent i = new Intent(getApplicationContext(), FirstPage.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(i);
     }
 }
